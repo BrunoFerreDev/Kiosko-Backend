7287fa3ec1a3ad31d547ab80642bbc31bff4ef44
Bruno Ferreira <pittanaferreira@gmail.com>
2026-08-12 02:42:28 -0300

    feat: configure Redis caching and update application properties; add PageDTO and RedisConfig classes

--- src/main/java/com/kiosco/KioscoApplication.java
+++ src/main/java/com/kiosco/KioscoApplication.java
@@ -2,8 +2,16 @@ package com.kiosco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootApplication
+@EnableCaching
 public class KioscoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/kiosco/dto/PageDTO.java
@@ -0,0 +1,28 @@
+package com.kiosco.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class PageDTO<T> implements Serializable {
+    private List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public PageDTO(Page<T> page) {
+        this.content = page.getContent();
+        this.pageNumber = page.getNumber();
+        this.pageSize = page.getSize();
+        this.totalElements = page.getTotalElements();
+        this.totalPages = page.getTotalPages();
+        this.last = page.isLast();
+    }
+}
